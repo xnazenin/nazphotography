24257422de6f0f8686787e5e4b3218744bd9ffe5
xnazenin <nazaninkhasmammadova@gmail.com>
2024-09-18 11:52:42 +0300

    Added back to top button functionality

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-//JavaScript to toggle the navigation menu -->
-
 const menuIcon = document.getElementById('menu-icon');
 const navList = document.getElementById('nav-list');
 const galleryImages = document.querySelectorAll('.gallery-item img');
+const backToTopButton = document.getElementById('backToTop');
 
+// Toggle the navigation menu
 menuIcon.addEventListener('click', () => {
     navList.classList.toggle('active');
 });
@@ -14,4 +14,19 @@ galleryImages.forEach(image => {
         // Open the image source in a new tab
         window.open(this.src, '_blank');
     });
+});
+
+// Back to Top button functionality
+backToTop.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+  	document.documentElement.scrollTop = 0;
+});
+
+// Show the button when the user scrolls down 100px
+window.addEventListener('scroll', function () {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
 });
